d85d442af08148ef3bd95c3dd9c77da5c8dbb680
zeeskii6 <zanyrie.wilson@carvercollegiate.org>
2025-11-05 18:50:23 +0000

    bouncing

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -31,17 +31,48 @@ $(function () {
 
 
     // TODO 2 - Create Platforms
-   createPlatform();
+  
+createPlatform(300,600,100,10,"green");
+createPlatform(200,700,100,10);
+createPlatform(400,500,100,10,"purple");
+createPlatform(500,400,100,10);
+createPlatform(600,400,100,10);
+createPlatform(700,500,100,10,"green");
+createPlatform(200,700,100,10,"pink");
+createPlatform(800,400,100,10,"blue");
+createPlatform(900,700,100,10,"orange");
+createPlatform(400,900,100,10);
+createPlatform(200,500,100,10);
+createPlatform(100,400,100,10);
+createPlatform(300,200,100,10);
+createPlatform(200,300,100,10);
+createPlatform(100,400,100,10);
+createPlatform(400,100,100,10);
+createPlatform(100,400,100,10);
+
+
+
+
+
+
+
+
+
+
 
 
 
     // TODO 3 - Create Collectables
+    createCollectable("max",350,550,0.5,0.7);
+    createCollectable("max",750,460,0.5,0.7);
+    createCollectable("max",100,40,0.5,0.7);
+    createCollectable("max",480,195,0.5,0.7);
 
 
 
     
     // TODO 4 - Create Cannons
-
+    createCannon("left",300,2000);
 
     
     
